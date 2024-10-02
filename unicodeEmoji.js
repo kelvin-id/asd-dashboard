edcb35e7b7b9062e84426ee56a8d0e45fe2566da
kwuite <kelvin@ludaro.nl>
2024-10-02 04:41:47 +0200

    Now we use the correct JavaScript Unicode escape sequence our unicode emoji's

--- unicodeEmoji.js
+++ unicodeEmoji.js
@@ -1,20 +1,20 @@
 const emojiList = {
-    puzzle: { icon: '🧩', unicode: '\uD83E\uDDF9', description: 'Widget to grid' },
-    satellite: { icon: '📡', unicode: '\uD83D\uDCF1', description: 'Serviceworker' },
-    cross: { icon: '❌', unicode: '\u274C', description: 'Delete widget' },
-    arrowDown: { icon: '⬇', unicode: '\u2B07', description: 'Increase height' },
-    arrowRight: { icon: '➡', unicode: '\u27A1', description: 'Increase width' },
-    arrowUp: { icon: '⬆', unicode: '\u2B06', description: 'Decrease height' },
-    arrowLeft: { icon: '⬅', unicode: '\u2B05', description: 'Decrease width' },
-    magnifyingGlass: { icon: '🔍', unicode: '\uD83D\uDD0D', description: 'Search' },
-    wrench: { icon: '🔧', unicode: '\uD83D\uDD27', description: 'Tune' },
-    link: { icon: '🔗', unicode: '\uD83D\uDD17', description: 'Link' },
-    ruler: { icon: '📏', unicode: '\uD83D\uDCCF', description: 'Adjust widget size' }, // Corrected Unicode
-    triangularRuler: { icon: '📐', unicode: '\uD83D\uDCD0', description: 'Adjust widget size' }, // Corrected Unicode
-    toolbox: { icon: '🧰', unicode: '\uD83E\uDDF0', description: 'Toolbox' }, // Corrected Unicode
-    hammerAndWrench: { icon: '🛠️', unicode: '\uD83D\uDEE0', description: 'Wrench' },
-    compass: { icon: '🧭', unicode: '\uD83E\uDDED', description: 'Compass' },
-    globe: { icon: '🌐', unicode: '\uD83C\uDF10', description: 'Globe' },
+    puzzle: { icon: '🧩', unicode: '\u{1F9E9}', description: 'Widget to grid' },
+    satellite: { icon: '📡', unicode: '\u{1F4E1}', description: 'Serviceworker' },
+    cross: { icon: '❌', unicode: '\u{274C}', description: 'Delete widget' },
+    arrowDown: { icon: '⬇', unicode: '\u{2B07}', description: 'Increase height' },
+    arrowRight: { icon: '➡', unicode: '\u{27A1}', description: 'Increase width' },
+    arrowUp: { icon: '⬆', unicode: '\u{2B06}', description: 'Decrease height' },
+    arrowLeft: { icon: '⬅', unicode: '\u{2B05}', description: 'Decrease width' },
+    magnifyingGlass: { icon: '🔍', unicode: '\u{1F50D}', description: 'Search' },
+    wrench: { icon: '🔧', unicode: '\u{1F527}', description: 'Tune' },
+    link: { icon: '🔗', unicode: '\u{1F517}', description: 'Link' },
+    ruler: { icon: '📏', unicode: '\u{1F4CF}', description: 'Adjust widget size' }, 
+    triangularRuler: { icon: '📐', unicode: '\u{1F4D0}', description: 'Adjust widget size' },
+    toolbox: { icon: '🧰', unicode: '\u{1F9F0}', description: 'Toolbox' },
+    hammerAndWrench: { icon: '🛠️', unicode: '\u{1F6E0}', description: 'Wrench' },
+    compass: { icon: '🧭', unicode: '\u{1F9ED}', description: 'Compass' },
+    globe: { icon: '🌐', unicode: '\u{1F310}', description: 'Globe' },
 };
 
 export default emojiList;
