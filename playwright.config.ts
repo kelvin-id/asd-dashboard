1ecca9b845e86bc7e738ec362f45fee6138b629d
kwuite <kelvin@ludaro.nl>
2024-10-12 04:50:35 +0200

    Playwright CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -27,7 +20,7 @@ export default defineConfig({
   // Faster feedback and less spend minutes in Githun Actions
   timeout: 10000, // Timeout for each test (in milliseconds) default is 30000
   expect: {
-      timeout: 2000, // Timeout for `expect` assertions
+      timeout: 4000, // Timeout for `expect` assertions
   },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
